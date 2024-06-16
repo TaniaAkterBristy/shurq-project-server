5281616fd479873408930aee351c7d757e896c73
Tania Akter Bristy <taniaakterbristybd@gmail.com>
2024-06-16 18:29:04 +0600

    cors client side link added

--- server.js
+++ server.js
@@ -9,14 +9,22 @@ const db = require('./routes/userRoute')
 const webRouter = require('./routes/webRoute')
 
 const app = express();
-app.use(express.json())
+
 
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
-app.use(cors())
+const corsOptions = {
+    origin: [
+        'http://localhost:3000',
+        'https://shurq-project-client.vercel.app'
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
 
 
 // middleware
